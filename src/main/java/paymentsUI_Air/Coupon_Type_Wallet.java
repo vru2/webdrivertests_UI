fe5abbf6855a9203274b3bdae8cfd2999f79fcae
Kiran Kumar <kiran.kumar@cleartrip.com>
2020-10-07 18:45:34 +0530

    wallet coupon fix

---
+++ src/main/java/paymentsUI_Air/Coupon_Type_Wallet.java
@@ -0,0 +1,45 @@
+// Framework - Cleartrip Automation
+// Author - Kiran Kumar
+
+package paymentsUI_Air;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Coupon_Type_Wallet extends PaymentUI_Common{
+	public RemoteWebDriver driver;
+	
+	@Test (priority=1)
+	public void Coupon_Wallet() throws Exception {		
+		String PayUrl = getPayUI("AirCouponWT", "");
+		driver.manage().deleteAllCookies(); 
+		driver.get(PayUrl);
+		payUI_Select_PaymentType(driver, "CC");
+		if(textPresent(driver, "Coupon code (DOMOW)", 5)) {
+			Reporter.log("Wallet coupon details are shown");
+			Assert.assertTrue(false);
+		}	
+	}
+	
+	@BeforeClass
+	public void setUp() throws Exception {
+		driver=(RemoteWebDriver) getDriver(driver);
+	}
+
+	@AfterMethod (alwaysRun = true)
+	public void afterMethod(ITestResult _result) throws Exception {
+		afterMethod(driver, _result);
+	}
+	  
+	@AfterClass
+	public void tearDown() throws Exception {
+		browserClose(driver);
+	}
+	
+}

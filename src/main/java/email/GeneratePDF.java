34d32977ea2983ad9896946d575538525a054c86
varalakshmiv <varalakshmi.venkateshaiah@cleartrip.com>
2020-10-08 14:16:50 +0530

    TS and Comm API fixes

--- src/main/java/email/GeneratePDF.java
+++ src/main/java/email/GeneratePDF.java
@@ -13,6 +13,8 @@ public class GeneratePDF extends EmailCommon {
 		Response resp;
 		String url = "http://172.17.51.86:8001/email/getpdf";
 		resp=EmailgeneratePDF(params4,headersForEmailgeneratePKPasscall(),url);
+		System.out.println(resp.asString());
 		validationPDF(resp);
+		
 	}
 }

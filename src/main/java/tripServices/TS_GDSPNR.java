953a37d97a04aa1dd1419a2882214e27ae6867b8
varalakshmiv <varalakshmi.venkateshaiah@cleartrip.com>
2020-10-20 10:25:33 +0530

    Add gdspnr script

---
+++ src/main/java/tripServices/TS_GDSPNR.java
@@ -0,0 +1,37 @@
+package tripServices;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+
+public class TS_GDSPNR extends TripserviceCommon {
+	Response resp;
+	String url="http://172.17.51.86:9031/trips/gds-pnr/VJ5MZD";
+	
+	@Test
+	public void gdspnr()
+	{
+		Reporter.log(url);
+		resp=RestAssured.get(url);
+		Reporter.log(resp.asString());
+		System.out.println(resp.asString());
+		if(resp.getStatusCode()==200)
+		{
+			ResponseBody body= resp.getBody();
+		    String bodyAsString = body.asString();
+		    Assert.assertNotNull(bodyAsString);
+		    Assert.assertEquals(bodyAsString.contains("trip_ref"),true,"Response body contains trip_ref");
+		}
+		else {
+			Reporter.log("Status code : " + resp.statusCode());
+			assertTrue(false);
+		}
+	}
+
+}

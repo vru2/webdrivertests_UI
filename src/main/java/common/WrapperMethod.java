9b8c3aed59a19454109d4649bc30a805e43e15ee
Likhitha <ns.likhitha@cleartrip.com>
2021-08-31 18:01:16 +0530

    pwa

--- src/main/java/common/WrapperMethod.java
+++ src/main/java/common/WrapperMethod.java
@@ -373,7 +373,7 @@ public class WrapperMethod extends CommonUtil {
 						"--user-agent=Mozilla/5.0 (iPhone; U; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5");
 
 				driver = new ChromeDriver(options);
-
+				
 				driver.manage().deleteAllCookies();
 				// driver.navigate().refresh();
 				// driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
--- src/main/java/payments_PWA_UI/Mobile_PWA_NBflow.java
+++ src/main/java/payments_PWA_UI/Mobile_PWA_NBflow.java
@@ -48,7 +48,9 @@ public class Mobile_PWA_NBflow extends Mobile_PWA_Common
 	}
 	@BeforeClass
 	public void startSelenium() throws Exception {
-		driver=(RemoteWebDriver) getMobileDriver1(driver);
+		//driver=(RemoteWebDriver) getMobileDriver1(driver);
+		
+		this.driver = getMobileDriver1(driver);
 		baseUrl = getBaseUrl("com");
 		driver.manage().window().maximize();
 

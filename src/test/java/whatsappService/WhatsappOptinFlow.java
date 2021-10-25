c7897efe9a71d968cba8f1027e07f77fd2a1bc72
Varalakshmi <varalakshmi.venkateshaiah@cleartrip.com>
2021-10-25 12:48:09 +0530

    Framework Enhancement

--- src/test/java/whatsappService/WhatsappOptinFlow.java
+++ src/test/java/whatsappService/WhatsappOptinFlow.java
@@ -11,7 +11,6 @@ public class WhatsappOptinFlow extends WhatsappCommon{
 	public void message() throws IOException
 	{
 		Response resp;
-		//String url = "http://whatsappapi.cltp.com:9001/r3/whatsapp/optin";
 		String url ="http://172.17.51.86:8227/whatsapp/optin";
 		resp=paramsForwhatsappOptin(headersForWhatsappUserMessage(),params3,url);
 		validateOptinFlow(resp);

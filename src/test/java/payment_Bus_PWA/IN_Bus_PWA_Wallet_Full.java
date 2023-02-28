40adbd0722ce521c04d9a486cd4773b3274d06b0
Kiran Kumar K <kiran.kumar4@BLRETV-C02GK44Z.local>
2023-02-28 12:20:44 +0530

    downgraded teestng

---
+++ src/test/java/payment_Bus_PWA/IN_Bus_PWA_Wallet_Full.java
@@ -0,0 +1,41 @@
+package test.java.payment_Bus_PWA;
+
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import test.java.commonUI.PaymentsUI_Common_PWA_Bus;
+
+
+public class IN_Bus_PWA_Wallet_Full extends PaymentsUI_Common_PWA_Bus {
+
+    @BeforeClass
+    public void startSelenium() throws Exception {
+        this.driver =  getMobileDriver(driver);
+    }
+
+    @Test
+    public void Bus_PWA_WalletFull() throws Exception {
+        driver.get(bus_SRPUrl("IN", "", "", 8));
+        bus_SRP(driver, "", "");
+        bus_SeatSelection(driver, "", "");
+        bus_SelectPickup(driver, "", "");
+        bus_TravellerPage(driver, "", phoneNo, emailID);
+        driver.manage().addCookie(fullwallet);
+        bus_ReviewPage(driver, "", "", "");
+        bento_Paymentpage_PWA(driver,"WALLET","","","","", "");
+        bus_ConfirmationPage(driver, "","", "Bus Wallet Full");
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void closeSelenium() throws Exception {
+        browserClose(driver);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void afterMethod(ITestResult _result) throws Exception {
+        afterMethod(driver, _result);
+    }
+
+}
